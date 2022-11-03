d1341a66ccbc0a488bc1ca7c094fc388194267bc
jomulagy <jomulagy988@gmail.com>
2022-11-03 17:09:34 +0900

    _deal : model, form 생성

--- _deal/models.py
+++ _deal/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from _account.models import User
+from _product.models import PRODUCT_TYPES, Product
 
-# Create your models here.
+DEAL_STATES = (
+    ("WAIT", "대기중"),
+    ("LEND", "대여중"),
+    ("TERMINATE", "종료")
+)
+class Deal(models.Model):
+    #foreignkye field
+    user_cons = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_prod = models.ForeignKey(User, on_delete=models)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    start_date = models.DateTimeField(null = False)
+    end_date = models.DateTimeField(null = False)
+    type = models.CharField(null = False, max_length = 20, choices = PRODUCT_TYPES)
+    state = models.CharField(null = False, default = "WAIT", max_length = 20, choices = DEAL_STATES)
