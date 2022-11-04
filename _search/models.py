74ca0169384eb5c98c9d753c3501d9a70e5b4897
jomulagy <jomulagy988@gmail.com>
2022-11-04 16:43:06 +0900

    검색 일자 조회 함수 추가

--- _search/models.py
+++ _search/models.py
@@ -4,3 +4,11 @@ class Keyword(models.Model):
     count = models.IntegerField(null = True, default = 0)
     date = models.DateTimeField(auto_now_add = True)
     content = models.CharField(null = False, max_length = 100)
+
+    def get_date(self):
+        str = self.date.strftime("%m.%d")
+        
+        return str
+    
+    def searched(self):
+        self.count+=1
