c26f444b22d599d7c35e25730eaf5857a0b7d277
jomulagy <jomulagy988@gmail.com>
2022-11-06 16:51:06 +0900

    메인 페이지 백엔드 완

--- _barrow/views.py
+++ _barrow/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from _search.models import Keyword
 from _account.models import User
+from _product.models import Product
+from django.db.models import Sum
 
+import datetime
+from datetime import timedelta
 
 def base(request):
     # user = request.user
@@ -10,7 +14,6 @@ def base(request):
         #검색기록
         recent_search_query = user.keyword.all().order_by("-date")
         recent_search_query = recent_search_query[:10]
-        
         
         most_search_query = Keyword.objects.all().order_by('-count')
         most_search_query = most_search_query[:20]
@@ -30,20 +33,32 @@ def side(request):
     favorite_num = favorite.count()
 
     recent_view = user.get_recent_view()
-    print(recent_view)
 
     side_context = {
         "favorite_num" : favorite_num,
         "recent_views" : recent_view,
     }
     return side_context
+
+def get_best():
+    products = Product.objects.all().order_by("-views")
+    
+    return products
+
 def home(request):
     # if request.user.is_authenticated:
 
         # user = request.user
         user = User.objects.get(id = "1")
 
-        context = {}
+        best_products = get_best()
+        best_products = best_products[:4]
+
+        context = {
+            "best_products" : best_products,
+        }
         context.update(base(request))
         context.update(side(request))
+
+        
         return render(request, "test.html", context)
--- config/settings.py
+++ config/settings.py
@@ -128,9 +128,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
-
-DATETIME_FORMAT = '%Y-%m-%d'
+USE_TZ = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
