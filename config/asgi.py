2e877425487519dec5013450f53508d1a68e00eb
jihoon kim <87347699+jomulagy@users.noreply.github.com>
2022-11-30 00:24:04 +0900

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -14,7 +14,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from barrow._chatting import routing
+from _chatting import routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
