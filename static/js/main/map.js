58df2e9e29c7f3d5776e0e5381f9a56cdf17bbdd
seokhyun Yoon <gah1234@naver.com>
2022-12-03 23:51:47 +0900

    main_page
    최근 검색어 css
    지도 css
    지도 페이지 네이션
    검색했을떄
    에러나는 이유 찾기 -> 해결

--- static/js/main/map.js
+++ static/js/main/map.js
@@ -5,122 +5,6 @@ var staticlon = 0;
 
 
 
-//페이지네이션 ----------------------------------------------------------------------
-const rowsPerPage = 20;
-const rowz = document.querySelectorAll(".bestitemlink");
-const rows = document.querySelectorAll('.bestitem');
-const rowsCount = rows.length; //100/8  12.9 -> 13
-const pageCount = Math.ceil(100/20);
-const numbers = document.querySelector('#numbers');
-
-const prevPageBtn = document.querySelector('.prevPageBtns');
-const nextPageBtn = document.querySelector('.nextPageBtns');
-console.log(prevPageBtn);
-let pageActiveIdx = 0; //현재 보고 있는 페이지 그룹 번호
-let currentPageNum = 0; //현재 보고 있는 페이지 번호
-let maxPageNum = 9; //페이지그룹 최대 개수
-console.log(numbers);
-
-for(let i=1; i<=pageCount; i++){
-   numbers.innerHTML += `<li><a href="">${i}</a></li>`;
-}
-
-const numberBtn = numbers.querySelectorAll('a');
-console.log(numberBtn);
-
-for(nb of numberBtn){
-  nb.style.display="none";
-}
-
-numberBtn.forEach((item,idx) => {
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
-      for(nb of numberBtn){
-        nb.classList.remove('active');
-      }
-      e.target.classList.add('active');
-      displayRow(idx);
-    });
-});
-function displayRow(idx){
-  let start = idx*rowsPerPage;
-  let end = start+rowsPerPage;
-
-  let rowsArray = [...rows];
-  for(ra of rowsArray){
-    ra.style.display = 'none';
-  }
-
-  let newRows = rowsArray.slice(start,end);
-  for(nr of newRows){
-    nr.style.display = '';
-  }
-  for(nb of numberBtn){
-    nb.classList.remove('active');
-  }
-  console.log(numberBtn[idx]);
-  numberBtn[idx].classList.add('active');
-}
-
-displayRow(0);
-
-//페이지네이션 그룹 표시 함수
-function displayPage(num){
-  //페이지네이션 번호 감추기
-  for(nb of numberBtn){
-    nb.style.display ='none';
-  }
-  let totalPageCount = Math.ceil(pageCount/maxPageNum);
-  console.log(totalPageCount);
-  console.log(pageActiveIdx);
-  let pageArr = [...numberBtn];
-  let start = num*maxPageNum;
-  let end = start+maxPageNum;
-  let pageListArr = pageArr.slice(start, end);
-
-  for(let item of pageListArr){
-    item.style.display="block";
-  }
-
-  if(pageActiveIdx == 0) {
-    prevPageBtn.style.display = 'none'
-  } else {
-    prevPageBtn.style.display = 'flex'
-  }
-  if(pageActiveIdx == totalPageCount -1) {
-    nextPageBtn.style.display = 'none'
-  } else {
-    nextPageBtn.style.display = 'flex'
-  }
-}
-displayPage(0);
-const dLeftVector = document.querySelector(".dLeftVector");
-const dRightVector = document.querySelector(".dRightVector");
-if(pageActiveIdx !== 0) {
-  dRightVector.addEventListener('click', () => {
-    let nextPageNum = pageActiveIdx * maxPageNum + maxPageNum;
-    displayRow(nextPageNum);
-    ++pageActiveIdx;
-    displayPage(pageActiveIdx);
-  });
-  dLeftVector.addEventListener('click', () => {
-    let nextPageNum = pageActiveIdx * maxPageNum - maxPageNum;
-    displayRow(nextPageNum);
-    --pageActiveIdx;
-    displayPage(pageActiveIdx);
-  });
-}
-nextPageBtn.addEventListener('click', () => {
-  pageActiveIdx++;
-  displayRow(pageActiveIdx);
-  displayPage(pageActiveIdx);
-});
-
-prevPageBtn.addEventListener('click', () => {
-  pageActiveIdx--;
-  displayRow(pageActiveIdx);
-  displayPage(pageActiveIdx);
-});
 // nextPageBtn.addEventListener('click',() => {
 //   let nextPageNum = pageActiveIdx;
 //   displayRow(nextPageNum);
@@ -239,3 +123,125 @@ function gido(){
         }
         
     }
+
+//페이지네이션 ----------------------------------------------------------------------
+
+const rowsPerPage = 20;
+const rows = document.querySelectorAll('.bestitem');
+const rowsCount = rows.length; //100/8  12.9 -> 13
+const pageCount = Math.ceil(item_num/20);
+const numbers = document.querySelector('#numbers');
+console.log(`itemnum:${item_num}`);
+const prevPageBtn = document.querySelector('.prevPageBtns');
+const nextPageBtn = document.querySelector('.nextPageBtns');
+console.log(pageCount);
+
+let pageActiveIdx = 0; //현재 보고 있는 페이지 그룹 번호
+let currentPageNum = 0; //현재 보고 있는 페이지 번호
+let maxPageNum = 9; //페이지그룹 최대 개수
+console.log(numbers);
+if(rows === 0){
+       numbers.innerHTML += `<li><a href="">${1}</a></li>`;
+}
+for(let i=1; i<=pageCount; i++){
+   numbers.innerHTML += `<li><a href="">${i}</a></li>`;
+}
+
+const numberBtn = numbers.querySelectorAll('a');
+console.log(numberBtn);
+
+for(nb of numberBtn){
+  nb.style.display="none";
+}
+
+numberBtn.forEach((item,idx) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+      for(nb of numberBtn){
+        nb.classList.remove('active');
+      }
+      e.target.classList.add('active');
+      displayRow(idx);
+    });
+});
+function displayRow(idx){
+  let start = idx*rowsPerPage;
+  let end = start+rowsPerPage;
+
+  let rowsArray = [...rows];
+
+  for(ra of rowsArray){
+    ra.style.display = 'none';
+  }
+
+  let newRows = rowsArray.slice(start,end);
+  for(nr of newRows){
+    nr.style.display = '';
+  }
+  for(nb of numberBtn){
+    nb.classList.remove('active');
+  }
+  console.log(numberBtn[idx]);
+  numberBtn[idx].classList.add('active');
+}
+
+displayRow(0);
+
+//페이지네이션 그룹 표시 함수
+function displayPage(num){
+  //페이지네이션 번호 감추기
+  for(nb of numberBtn){
+    nb.style.display ='none';
+  }
+  let totalPageCount = Math.ceil(pageCount/maxPageNum);
+  console.log(totalPageCount);
+  console.log(pageActiveIdx);
+  let pageArr = [...numberBtn];
+  let start = num*maxPageNum;
+  let end = start+maxPageNum;
+  let pageListArr = pageArr.slice(start, end);
+
+  for(let item of pageListArr){
+    item.style.display="block";
+  }
+
+  if(pageActiveIdx == 0) {
+    prevPageBtn.style.display = 'none'
+  } else {
+    prevPageBtn.style.display = 'flex'
+  }
+  if(pageActiveIdx == totalPageCount -1) {
+    nextPageBtn.style.display = 'none'
+  } else {
+    nextPageBtn.style.display = 'flex'
+  }
+}
+displayPage(0);
+const dLeftVector = document.querySelector(".dLeftVector");
+const dRightVector = document.querySelector(".dRightVector");
+if(pageActiveIdx !== 0) {
+  dRightVector.addEventListener('click', () => {
+    let nextPageNum = pageActiveIdx * maxPageNum + maxPageNum;
+    displayRow(nextPageNum);
+    ++pageActiveIdx;
+    displayPage(pageActiveIdx);
+  });
+  dLeftVector.addEventListener('click', () => {
+    let nextPageNum = pageActiveIdx * maxPageNum - maxPageNum;
+    displayRow(nextPageNum);
+    --pageActiveIdx;
+    displayPage(pageActiveIdx);
+  });
+}
+
+nextPageBtn.addEventListener('click', () => {
+  pageActiveIdx++;
+  displayRow(pageActiveIdx);
+  displayPage(pageActiveIdx);
+});
+
+prevPageBtn.addEventListener('click', () => {
+  pageActiveIdx--;
+  displayRow(pageActiveIdx);
+  displayPage(pageActiveIdx);
+});
