bd29934ae905de682da340dddd425cd66489da22
HYERINI <rinlee2001@naver.com>
2022-12-29 00:28:39 +0900

    hyerin

--- _account/models.py
+++ _account/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from phonenumber_field.modelfields import PhoneNumberField
 from _product.models import Product
 
 from datetime import datetime
--- config/settings.py
+++ config/settings.py
@@ -48,8 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'phonenumber_field',
-
 
     '_account',
     '_barrow',
